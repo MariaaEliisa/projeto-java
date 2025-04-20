a3de886b563553493d929f8dd56fd4aa8b465bba
MariaaEliisa <162669316+MariaaEliisa@users.noreply.github.com>
2025-04-20 01:24:31 -0300

    Add files via upload

---
+++ ProjJava/src/ProdutoFactory.java
@@ -0,0 +1,7 @@
+public class ProdutoFactory {
+    public static Produto criarProduto(int id, String tipo, String descricao, double preco) {
+        return new Produto(id, tipo, descricao, preco);
+    }
+}
+
+//p.p
